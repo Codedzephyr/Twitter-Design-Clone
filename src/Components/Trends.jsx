d5334d043e7a147b808f7b3491ea51a4d13495ef
Codedzephyr <salihu45@gmail.com>
2022-06-10 11:16:23 +0100

    initial commit

--- src/Components/Trends.jsx
+++ src/Components/Trends.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Trends = () => {
+  const [blue, setBlue] = useState(false);
+
+  const changeColour = () => {
+    setBlue(!blue);
+  };
   return (
     <React.Fragment>
       <div className="trendsContainer">
@@ -10,7 +15,9 @@ const Trends = () => {
               viewBox="0 0 24 24"
               aria-hidden="true"
               height="1.75rem"
-              fill="rgb(113, 118, 123)"
+              className={
+                blue ? "searchBackground" : "serachBackgroundOriginal"
+              }
             >
               <g>
                 <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
@@ -18,7 +25,13 @@ const Trends = () => {
             </svg>
           </div>
           <div className="searchInputContainerMini">
-            <input type="text" id="search" placeholder="Search Twitter"></input>
+            <input
+              type="text"
+              id="search"
+              placeholder="Search Twitter"
+              onFocus={changeColour}
+              onBlur = {changeColour}
+            ></input>
           </div>
         </div>
         <div className="listOfTrendsContainer">
